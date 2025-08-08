39a76260448bb8d98f710fe977b30c374b805516
hyewonny2327 <wonny2327@gmail.com>
2025-08-08 16:05:08 +0900

    #6 refactor : className 기본값으로 단순화

--- src/components/common/Chip.tsx
+++ src/components/common/Chip.tsx
@@ -2,11 +2,10 @@ type ChipProps = {
   children: React.ReactNode;
   className?: string;
 };
-const Chip = ({ children, className }: ChipProps) => {
-  const chipStyle = className ? className : '';
+const Chip = ({ children, className = '' }: ChipProps) => {
   return (
     <div
-      className={`flex items-center justify-center rounded-full border border-[var(--color-text-muted)] bg-[var(--color-button-sand)] px-4 py-2 text-lg font-medium text-[var(--color-text-muted)] ${chipStyle}`}
+      className={`flex items-center justify-center rounded-full border border-[var(--color-text-muted)] bg-[var(--color-button-sand)] px-4 py-2 text-lg font-medium text-[var(--color-text-muted)] ${className}`}
     >
       {children}
     </div>
